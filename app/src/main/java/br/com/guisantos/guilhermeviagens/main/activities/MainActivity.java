f77e7b0502989f3504e6bed4c230a8e34f212598
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-04-20 17:51:03 -0300

    finaliza binding dos dados de acordo com o clicado que é passado via extra na intent

--- app/src/main/java/br/com/guisantos/guilhermeviagens/main/activities/MainActivity.java
+++ app/src/main/java/br/com/guisantos/guilhermeviagens/main/activities/MainActivity.java
@@ -2,7 +2,10 @@ package br.com.guisantos.guilhermeviagens.main.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.List;
@@ -13,15 +16,37 @@ import br.com.guisantos.guilhermeviagens.main.dao.PacoteDAO;
 import br.com.guisantos.guilhermeviagens.main.models.Pacote;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listaDePacotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle(R.string.pacotes_app_title);
-
-        ListView listaDePacotes = (ListView) findViewById(R.id.activity_main_lista_pacotes);
         List<Pacote> pacotes = new PacoteDAO().lista();
-        listaDePacotes.setAdapter(new PacotesAdapter(pacotes, this));
+
+        this.listaDePacotes = findViewById(R.id.activity_main_lista_pacotes);
+        this.listaDePacotes.setAdapter(new PacotesAdapter(pacotes, this));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.configuraClickItemListView();
+    }
+
+    private void configuraClickItemListView() {
+        this.listaDePacotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                abreResumoPacote((Pacote) adapterView.getItemAtPosition(position));
+            }
+        });
+    }
+
+    private void abreResumoPacote(Pacote pacoteSelecionado) {
+        Intent vaiParaResumoDoPacote = new Intent(MainActivity.this, ResumoPacoteActivity.class);
+        vaiParaResumoDoPacote.putExtra("pacote", pacoteSelecionado);
+        startActivity(vaiParaResumoDoPacote);
     }
 }
--- app/src/main/java/br/com/guisantos/guilhermeviagens/main/activities/ResumoPacoteActivity.java
+++ app/src/main/java/br/com/guisantos/guilhermeviagens/main/activities/ResumoPacoteActivity.java
@@ -2,16 +2,61 @@ package br.com.guisantos.guilhermeviagens.main.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import br.com.guisantos.guilhermeviagens.R;
+import br.com.guisantos.guilhermeviagens.main.models.Pacote;
+import br.com.guisantos.guilhermeviagens.main.utils.MoedaUtil;
 
 public class ResumoPacoteActivity extends AppCompatActivity {
+
+    private Pacote pacote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
         setTitle("Resumo do Pacote");
+        this.pacote = (Pacote) this.getIntent().getSerializableExtra("pacote");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.setaImagem(); //Seta imagem no header
+        this.setaTitleCidade();
+        this.setaDias();
+        this.setaPreco();
+    }
+
+    private void setaImagem() { //Seta imagem no header
+        ImageView imagem = findViewById(R.id.resumo_pacote_header_cidade);
+        Resources resources = this.getResources();
+        int idImagem = resources.getIdentifier(this.pacote.getImagem(), "drawable", this.getPackageName());
+        imagem.setImageResource(idImagem);
+    }
+
+    private void setaTitleCidade() {
+        TextView textoCidade = findViewById(R.id.resumo_pacote_cidade_title);
+        textoCidade.setText(this.pacote.getLocal());
+    }
+
+    private void setaDias() {
+        TextView textoDias = findViewById(R.id.resumo_pacote_quantidade_dias);
+        if(this.pacote.getDias() > 1)
+            textoDias.setText(this.pacote.getDias() + "dias");
+        else
+            textoDias.setText(this.pacote.getDias() + "dias");
+    }
+
+    private void setaPreco() {
+        TextView textoPreco = findViewById(R.id.resumo_pacote_preco);
+        textoPreco.setText(MoedaUtil.formataParaBrasileiro(this.pacote.getPreco()));
     }
 }
