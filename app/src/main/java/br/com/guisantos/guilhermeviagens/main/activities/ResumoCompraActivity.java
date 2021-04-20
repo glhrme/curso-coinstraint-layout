be5f0ad86027db83084f5df395386f8b9aef631b
Guilherme dos Santos <guilherme@guisantos.com.br>
2021-04-20 20:28:05 -0300

    finaliza layout resumo da compra

---
+++ app/src/main/java/br/com/guisantos/guilhermeviagens/main/activities/ResumoCompraActivity.java
@@ -0,0 +1,16 @@
+package br.com.guisantos.guilhermeviagens.main.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.guisantos.guilhermeviagens.R;
+
+public class ResumoCompraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_compra);
+    }
+}
